81056155ddc919fa79b332a5a03bbc0a78215c61
noFonte <deolhonofonte@gmail.com>
2025-04-18 17:02:11 -0300

    resolução do UserCases Mãe

---
+++ nursery-control/domain/UseCases/UpdateMotherUseCase.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Domain\UseCases;
+
+use Application\MotherInput;
+use Infra\Repositories\Entities\IMotherRepository;
+
+
+
+class UpdateMotherUseCase{
+
+    private IMotherRepository $motherRepository;
+
+    public  function __construct(IMotherRepository $motherRepository){
+        $this->motherRepository =  $motherRepository;
+        
+    }
+
+    public function execute($code,MotherInput $mother){
+      
+        return  $this->motherRepository->update($code,$mother);
+
+    }
+}
