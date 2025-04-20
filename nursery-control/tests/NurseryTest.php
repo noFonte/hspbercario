ccff60a3899740f613d5a89d1cc7754e018ff7d1
Edilson <edilsonclsivs@gmail.com>
2025-04-20 07:28:20 -0300

    Criaçao do UserCase de Registro de Parto parte 1, foi criado os inputs e os repositorios

--- nursery-control/tests/NurseryTest.php
+++ nursery-control/tests/NurseryTest.php
@@ -1,9 +1,25 @@
 <?php
 
+use Application\DoctorInput;
+use Application\MotherInput;
 use Application\NewBornInput;
 use PHPUnit\Framework\TestCase;
+use Domain\UseCases\CreatedDoctorUseCase;
+use Domain\UseCases\CreatedMotherUseCase;
+use Domain\UseCases\CreatedNewBornUseCase;
+use Infra\Repositories\IDoctorRepository;
+use Infra\Repositories\IMotherRepository;
+use Infra\Repositories\INewBornInRepository;
 
 require_once("libs.php");
+
+require_once("RepositoriesInMemory/DoctorInMemoryRepository.php");
+require_once("RepositoriesInMemory/NewBornInMemoryRepository.php");
+require_once("RepositoriesInMemory/MotherInMemoryRepository.php");
+
+
+
+
 
 
 
@@ -11,6 +27,8 @@ require_once("libs.php");
 class NurseryAlreadyExistsException extends Exception {}
 
 class NurseryNotAlreadyExistsException extends Exception {}
+
+
 
 
 class NurseryInput
@@ -44,6 +62,51 @@ class NurseryInput
         return $props;
     }
 }
+
+
+
+
+
+class NewBirthInput
+{
+    private $inputs = ["code","dateofbirth", "associatedwithMother", "associatedwithDoctor"];
+    public $code;
+    public $dateofbirth;
+    public $associatedwithMother;
+    public $associatedwithDoctor;
+    public function __construct($input = null)
+    {
+
+        
+        if (!is_null($input) && is_array($input)) {
+
+            foreach ($this->inputs as $value) {
+                if (isset($input[$value])) {
+                    $this->$value = $input[$value];
+                }
+            }
+        }
+    }
+
+
+    public function toArray()
+    {
+
+        $props = [];
+
+        foreach ($this->inputs as $value) {
+            $props[$value] = $this->$value;
+        }
+        return $props;
+    }
+}
+
+
+
+
+
+
+
 
 class NurseryOutPut
 {
@@ -114,6 +177,25 @@ class NurseryEntity
         return $props;
     }
 }
+
+
+
+
+interface IChildbirthRepository{
+    function created(NewBirthInput $newBirth);
+}
+
+
+class ChildbirthInMemoryRepository  implements IChildbirthRepository{
+
+    private $tableChildbirth = [];
+    function created(NewBirthInput $newBirth){
+        
+        $this->tableChildbirth[] = new ChildbirthEntity($newBirth->toArray());
+        return true;
+    }
+}
+
 
 
 
@@ -210,6 +292,62 @@ class ActiveOrDesactiveNurseryUseCase
     }
 }
 
+
+
+
+class RegisteringANewBirthUseCase
+{
+    private INurseryRepository $iNurseryRepository;
+    private IMotherRepository $motherRepository;
+    private INewBornInRepository $newBornRepository;
+    private IDoctorRepository $iDoctorRepository;
+    private IChildbirthRepository $iChildbirthRepository;
+
+    public function __construct(
+        INurseryRepository $iNurseryRepository,
+        IMotherRepository $motherRepository,
+        INewBornInRepository $newBornRepository,
+        IDoctorRepository $iDoctorRepository,
+        IChildbirthRepository $iChildbirthRepository
+    ) {
+        $this->motherRepository = $motherRepository;
+        $this->newBornRepository = $newBornRepository;
+        $this->iDoctorRepository = $iDoctorRepository;
+        $this->iNurseryRepository = $iNurseryRepository;
+        $this->iChildbirthRepository = $iChildbirthRepository;
+    }
+    public function execute(NewBirthInput $newBirth) {
+
+        dd($newBirth);
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class NurseryTest extends TestCase
 {
     function testCreatedNursery()
@@ -284,5 +422,92 @@ class NurseryTest extends TestCase
         $output = $createdNurseryUseCase->execute($Nursery);
         $createdNurseryUseCase =  new CreatedNurseryUseCase($nurseryMemory);
         $output = $createdNurseryUseCase->execute($Nursery);
+    }
+
+
+    function testRegisteringANewBirth()
+    {
+
+        /*
+            Deve Adicionar um Novo  parto, Associando A Mãe e a Criança e o
+            MEdico que realizou o parto se o [**bercario estiver ativo]
+
+        */
+
+        $motherMemory = new MotherInMemoryRepository();
+        $doctorMemory = new DoctorInMemoryRepository();
+        $newbornMemory = new NewBornInMemoryRepository();
+        $nurseryMemory = new NurseryInMemoryRepository();
+        $childbirthMemory = new ChildbirthInMemoryRepository();
+        
+
+        $newbornBirth = date("Y") - 30;
+
+
+        $doctor = new DoctorInput(
+            [
+                "code" => uniqid(),
+                "name" => sprintf("Medico_%s", uniqid()),
+                "crm" => sprintf("crm-%s", uniqid()),
+                "phone" => rand(9999990, 9999999),
+                "cellphone" =>  rand(9999990, 9999999),
+                "specialty" =>  sprintf("habilidade-%s", uniqid()),
+
+            ]
+        );
+        $createddoctorUseCase =  new CreatedDoctorUseCase($doctorMemory);
+        $output = $createddoctorUseCase->execute($doctor);
+        $this->assertEquals(true, $output);
+        $mother = new MotherInput(
+            [
+                "code" => uniqid(),
+                "name" => sprintf("Mae_%s", uniqid()),
+                "address" => sprintf("Mae_%s_endereco", uniqid()),
+                "phone" => rand(9999990, 9999999),
+                "dateofbirth" => 1983
+            ]
+        );
+        $createdMotherUseCase =  new CreatedMotherUseCase($motherMemory);
+        $output = $createdMotherUseCase->execute($mother);
+        $this->assertEquals(true, $output);
+
+      
+
+        $newborn = new NewBornInput(
+            [
+                "code" => uniqid(),
+                "name" => sprintf("Bebê_%s", uniqid()),
+                "dateofbirth" => generateRandomDateTime(((date("Y") - $newbornBirth)), date("Y")),
+                "birthweight" => rand(1, 15),
+                "height" =>  rand(1, 60),
+                "associatedwithMother" => $mother->code,
+
+            ]
+        );
+        $createdNewBornUseCase =  new CreatedNewBornUseCase($newbornMemory);
+        $output = $createdNewBornUseCase->execute($newborn);
+        $this->assertEquals(true, $output);
+
+       
+
+        $newBirth = new NewBirthInput(
+            [
+                "code" => uniqid(),
+                "dateofbirth" => generateRandomDateTime(((date("Y") - $newbornBirth)), date("Y")),
+                "associatedwithMother" => $mother->code,
+                "associatedwithDoctor" => $doctor->code,
+            ]
+        );
+   
+
+        $registeringANewBirth =  new RegisteringANewBirthUseCase(
+            $nurseryMemory, $motherMemory, $newbornMemory, $doctorMemory,$childbirthMemory
+        );
+        $output = $registeringANewBirth->execute($newBirth);
+        $this->assertEquals(true, $output);
+
+
+
+        
     }
 }
