aa139b49af31cb83a07578ef3f0baf48526d269d
Edilson <edilsonclsivs@gmail.com>
2025-04-19 23:10:12 -0300

    User case bercario criados , Criacao, ativar e desativar e evitar duplicidade de registros

--- nursery-control/tests/NurseryTest.php
+++ nursery-control/tests/NurseryTest.php
@@ -8,6 +8,9 @@ require_once("libs.php");
 
 
 
+class NurseryAlreadyExistsException extends Exception {}
+
+class NurseryNotAlreadyExistsException extends Exception {}
 
 
 class NurseryInput
@@ -41,6 +44,45 @@ class NurseryInput
         return $props;
     }
 }
+
+class NurseryOutPut
+{
+    private $inputs = ["code",  "name", "state"];
+    public $code;
+    public $name;
+    public $state;
+    public function __construct($input = null)
+    {
+
+        if (!is_null($input) && is_array($input)) {
+
+            foreach ($this->inputs as $value) {
+                if (isset($input[$value])) {
+                    $this->$value = $input[$value];
+                }
+            }
+        }
+    }
+
+
+    public function toArray()
+    {
+
+        $props = [];
+
+        foreach ($this->inputs as $value) {
+            $props[$value] = $this->$value;
+        }
+        return $props;
+    }
+}
+
+
+
+
+
+
+
 class NurseryEntity
 {
     private $inputs = ["code",  "name", "state"];
@@ -76,18 +118,59 @@ class NurseryEntity
 
 
 
-interface INurseryRepository {
+interface INurseryRepository
+{
     function created(NurseryInput $nursery);
+    function active($code);
+    function desactive($code);
+    function byCode($code);
 }
 
-class NurseryInMemoryRepository implements INurseryRepository {
+class NurseryInMemoryRepository implements INurseryRepository
+{
 
-    private $tableNurserys=[];
+    private $tableNurserys = [];
 
-    function created(NurseryInput $nursery){
-        $this->tableNurserys[]=new NurseryEntity($nursery->toArray());
+    function created(NurseryInput $nursery)
+    {
+        $isNursery = $this->byCode($nursery->code);
+        if (!is_null($isNursery)) throw  new  NurseryAlreadyExistsException("duplicate record.");
+        $this->tableNurserys[] = new NurseryEntity($nursery->toArray());
         return true;
+    }
+    function active($code)
+    {
+        $nursery = $this->byCode($code);
+        if (is_null($nursery)) throw  new  NurseryNotAlreadyExistsException("nursery not exists.");
+        foreach ($this->tableNurserys as $key => $row) {
+            if ($row->code == $nursery->code && $row->state == true) {
+                $this->tableNurserys[$key]->state = false;
+                return true;
+            }
+        }
+    }
+    function desactive($code)
+    {
 
+        $nursery = $this->byCode($code);
+        if (is_null($nursery)) throw  new  NurseryNotAlreadyExistsException("nursery not exists.");
+        foreach ($this->tableNurserys as $key => $row) {
+            if ($row->code == $nursery->code && $row->state == false) {
+                $this->tableNurserys[$key]->state = true;
+                return true;
+            }
+        }
+
+      
+    }
+    function byCode($code)
+    {
+        foreach ($this->tableNurserys as $key => $row) {
+            if ($row->code == $code) {
+                return new NurseryOutPut($row->toArray());
+            }
+        }
+        return null;
     }
 }
 
@@ -100,14 +183,34 @@ class  CreatedNurseryUseCase
     {
         $this->iNurseryRepository =  $iNurseryRepository;
     }
-    public function execute(NurseryInput $nursery) {
+    public function execute(NurseryInput $nursery)
+    {
 
         return $this->iNurseryRepository->created($nursery);
-         
     }
 }
 
 
+
+
+
+class ActiveOrDesactiveNurseryUseCase
+{
+    private INurseryRepository $iNurseryRepository;
+
+    public function __construct(INurseryRepository $iNurseryRepository)
+    {
+        $this->iNurseryRepository =  $iNurseryRepository;
+    }
+    public function execute($code, $state)
+    {
+        if ($state) {
+            return $this->iNurseryRepository->desactive($code);
+        } else {
+            return $this->iNurseryRepository->active($code);
+        }
+    }
+}
 
 class NurseryTest extends TestCase
 {
@@ -121,7 +224,6 @@ class NurseryTest extends TestCase
 
 
         $nurseryMemory = new NurseryInMemoryRepository();
-
         $Nursery = new NurseryInput(
             [
                 "code" => uniqid(),
@@ -142,6 +244,51 @@ class NurseryTest extends TestCase
          Deve permitir Ativar e Desativar o Berçario , Deve Desativar o
          BErçario e não Deletar. pois tem Historicos
         */
+
+
+        $nurseryMemory = new NurseryInMemoryRepository();
+        $nursery = new NurseryInput(
+            [
+                "code" => uniqid(),
+                "name" => sprintf("Berçario_%s", uniqid()),
+                "state" => true
+            ]
+        );
+        $createdNurseryUseCase =  new CreatedNurseryUseCase($nurseryMemory);
+        $output = $createdNurseryUseCase->execute($nursery);
+        $this->assertEquals(true, $output);
+
+      
+        $activeOrDesactiveNurseryUseCase =  new ActiveOrDesactiveNurseryUseCase($nurseryMemory);
+        $output = $activeOrDesactiveNurseryUseCase->execute($nursery->code, false);
+        $this->assertEquals(true, $output);
+
+       
+        $activeOrDesactiveNurseryUseCase =  new ActiveOrDesactiveNurseryUseCase($nurseryMemory);
+        $output = $activeOrDesactiveNurseryUseCase->execute($nursery->code, true);
+        $this->assertEquals(true, $output);
+    }
+
+
+    function testFailHasNurseryRegistred()
+    {
+        $this->expectException(NurseryAlreadyExistsException::class);
+
+        $nurseryMemory = new NurseryInMemoryRepository();
+        $Nursery = new NurseryInput(
+            [
+                "code" => uniqid(),
+                "name" => sprintf("Berçario_%s", uniqid()),
+                "state" => true
+            ]
+        );
+        $createdNurseryUseCase =  new CreatedNurseryUseCase($nurseryMemory);
+        $output = $createdNurseryUseCase->execute($Nursery);
+        $createdNurseryUseCase =  new CreatedNurseryUseCase($nurseryMemory);
+        $output = $createdNurseryUseCase->execute($Nursery);
+
+
+
 
 
     }
