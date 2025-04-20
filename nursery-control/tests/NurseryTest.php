0923c35f5bb51be49650336e7056d87375765a39
Edilson <edilsonclsivs@gmail.com>
2025-04-19 22:10:58 -0300

    Teste do bercario Iniciado

---
+++ nursery-control/tests/NurseryTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class NurseryTest extends TestCase
+{
+    function testCreatedNursery()
+    {
+
+        /*
+        Criar um novo berçário se não existir, com os campos 
+        [code],[name],[state], o nome deve ser Unico como código
+        */
+    }
+
+
+    function testActivateOrDeactivateNursery()
+    {
+
+        /*
+         Deve permitir Ativar e Desativar o Berçario , Deve Desativar o
+         BErçario e não Deletar. pois tem Historicos
+        */
+
+
+    }
+
+
+    
+
+}
